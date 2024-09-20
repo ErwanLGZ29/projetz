62eede0a947b0c90926afc0936eb35fd39f3fac5
ErwanLGZ <erwan.lz@movensee.com>
2024-09-20 19:39:19 +0200

    feat(homepage): Create component cardDesc

---
+++ components/cardDesc.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="cart-Desc">
+        <img :src="imageSrc" :alt="imageAlt" class="card-image" />
+        <p>{{ description }}</p>
+    </div>
+  </template>
+
+<script>
+    export default {
+        name: 'cardDesc',
+        props: {
+            imageSrc: {
+                type: String,
+                required: true
+            },
+            imageAlt: {
+                type: String,
+                default: 'Card image'
+            },
+            description: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    .cart-Desc {
+        width: 100%;
+        background-color: var(--second-background-color);
+        color: var(--second-text-color);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 7rem;
+        border-radius: 1rem;
+        box-shadow: 5px 5px 10px #00093c8c;
+    }
+
+    .cart-Desc img {
+        width: 50%;
+        height: auto;
+        border-radius: 1rem;
+    }
+
+    .cart-Desc p {
+        width: 50%;
+        height: auto;
+        padding: 2rem;
+        font-size: 1.5rem;
+    }
+
+    @media screen and (max-width: 768px) {
+        .cart-Desc {
+            flex-direction: column;
+            margin-top: 4rem;
+        }
+
+        .cart-Desc img, .cart-Desc p{
+            width: 100%;
+            font-size: 1rem;
+        }
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,31 +5,44 @@
             <h1>Le Break Dance</h1>
         </div>
         <div class="desc-Panel">
-            <div className='desc-Panel-Article'>
-                <img src='/hong10.png' className="desc-image" alt="hong10" />
-                <p>Le breakdance, aussi appelé breaking est un style de danse développé à New York dans les années 1970,
-                caractérisé par ses mouvements de corps saccadés, son aspect acrobatique et ses figures au sol. Un danseur de breaking est appelé Bboy (pour un homme),
-                Bgirl (pour une femme).</p>
-            </div>
 
-            <div className='desc-Panel-Article'>
-                <p>Bien qu’on pense qu'il est né aux États-Unis, des archives montrent qu'une forme de breaking était dansée en Afrique,
-                plus exactement à Kaduna, au Nigéria, dans les années 19502.</p>
-                <img src='/carlota.jpg' className="desc-image" alt="carlota" />
-            </div>  
+            <cardDesc
+                imageSrc="/hong10.png"
+                imageAlt="hong10"
+                description="Le breakdance, aussi appelé breaking est un style de danse développé à New York dans les années 1970,
+                        caractérisé par ses mouvements de corps saccadés, son aspect acrobatique et ses figures au sol. Un danseur de breaking est appelé Bboy (pour un homme),
+                        Bgirl (pour une femme)."
+            />
 
-            <div className='desc-Panel-Article'>
-                <img src='/yuasa.jpg' className="desc-image" alt="yuasa" />
-                <p>En France, la première apparition officielle du breaking remonte à la tournée New York City Rap organisée par Europe 1 en 1982.
+            <cardDesc
+                imageSrc="/carlota.jpg"
+                imageAlt="carlota"
+                description="Bien qu’on pense qu'il est né aux États-Unis, des archives montrent qu'une forme de breaking était dansée en Afrique,
+                plus exactement à Kaduna, au Nigéria, dans les années 19502."
+            />
+
+            <cardDesc
+                imageSrc="/yuasa.jpg"
+                imageAlt="yuasa"
+                description="En France, la première apparition officielle du breaking remonte à la tournée New York City Rap organisée par Europe 1 en 1982.
                  En 1984, l'émission télévisée de Sydney, H.I.P. H.O.P. popularise la culture hip-hop en France;
-                 le jury de l'émission était les P.C.B. (Paris City Breakers).</p>
-            </div>  
+                 le jury de l'émission était les P.C.B. (Paris City Breakers)."
+            />
         </div>
     </div>
   </template>
 
-  
-  <style>
+<script>
+import cardDesc from './components/CardDesc.vue';
+
+export default {
+  components: {
+    cardDesc
+  }
+}
+</script>
+
+  <style scoped>
   @import url('https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700');
     .main-Panel{
         width: 100%;
@@ -60,30 +73,9 @@
        
     }
 
-    .desc-Panel-Article {
-        width: 100%;
-        background-color: var(--second-background-color);
-        color: var(--second-text-color);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-top: 7rem;
-        border-radius: 1rem;
-        box-shadow: 5px 5px 10px #00093c8c;
-    }
+    
 
-    .desc-Panel-Article img {
-        width: 50%;
-        height: auto;
-        border-radius: 1rem;
-    }
-
-    .desc-Panel-Article p {
-        width: 50%;
-        height: auto;
-        padding: 2rem;
-        font-size: 1.5rem;
-    }
+    
 
     @media screen and (max-width: 1300px) {
         .desc-Panel {
@@ -91,23 +83,13 @@
         }
     }
 
-    @media screen and (max-width: 650px) {
+    @media screen and (max-width: 768px) {
 
         h1 {
             font-size: 3rem;
         }
         .desc-Panel {
             width: 90%;
-        }
-
-        .desc-Panel-Article {
-            flex-direction: column;
-            margin-top: 4rem;
-        }
-
-        .desc-Panel-Article img, .desc-Panel-Article p{
-            width: 100%;
-            font-size: 1rem;
         }
     }
   </style>
