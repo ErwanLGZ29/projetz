3b0d000018ed1dbdf75ceedca0102f4282288b39
ErwanLGZ <erwan.lz@movensee.com>
2024-09-25 22:25:44 +0200

    ci(workflow): new test ci with nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt'],
+  modules: ["@pinia/nuxt"],
   ssr: true,
   vite: {
     server: {
       watch: {
-        usePolling: true
-      }
-    }
+        usePolling: true,
+      },
+    },
   },
   devtools: { enabled: true },
-  compatibilityDate: "2024-09-19"
-})
+  compatibilityDate: "2024-09-19",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+});
