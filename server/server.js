9216dcb696611d034464b263cd6e5c61eb21a972
ErwanLGZ <erwan.lz@movensee.com>
2024-09-28 17:08:10 +0200

    feat(server): fix old dep

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const path = require("path");
-import { sql  } from "@vercel/postgres";
+const { sql } = require('@vercel/postgres');
 
 const app = express();
 const PORT = 5000;
